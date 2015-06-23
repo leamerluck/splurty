722683aefe7cabcf99efc8a9cc757c52db2bee6c
vagrant <leamerluck@gmail.com>
2015-06-23 10:14:16 +0000

    Added test for initials w/ id

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,4 +1,11 @@
 class Quote < ActiveRecord::Base
 	validates :saying, :presence => true, :length => {:maximum => 240, :minimum => 3 }
 	validates :author, :presence => true, :length => {:maximum => 50, :minimum => 3 }
+
+	def unique_tag
+		initials = self.author.split.map do |sub_string|
+			sub_string[0]
+		end
+		initials.join + '#' + self.id.to_s
+	end
 end
